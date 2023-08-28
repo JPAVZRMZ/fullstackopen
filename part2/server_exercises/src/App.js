954774166e703daf35dfc0483c005a679b5a25b3
JPAVZRMZ <j.pablo.1012@hotmail.com>
2023-08-28 14:29:20 -0600

    Server with css and error notifications

--- part2/server_exercises/src/App.js
+++ part2/server_exercises/src/App.js
@@ -1,23 +1,40 @@
 import { useState, useEffect } from 'react'
-import axios from 'axios'
 import Note from './components/Note'
+import noteService from './services/notes'
+import Notification from './components/Notification'
+
+const Footer = () => {
+  const footerStyle = {
+    color: 'green',
+    fontStyle: 'italic',
+    fontSize: 12
+  }
+  return (
+    <div style={footerStyle}>
+      <br />
+      <em>Note app, Department of Computer Science, University of Helsinki 2023</em>
+    </div>
+  )
+}
+
 
 const App = () => {
   const [notes, setNotes] = useState([])
   const [newNote, setNewNote] = useState('a new note...')
   const [showAll, setShowAll] = useState(true)
+  const [errorMessage, setErrorMessage] = useState('')
 
 
   const hook = () => {
     console.log("effect")
-    axios
-      .get('http://localhost:3001/notes')
-      .then(response => {
+    noteService
+      .getAll('http://localhost:3001/notes')
+      .then(initialNotes => {
         console.log('promise fulfilled')
-        setNotes(response.data)
+        setNotes(initialNotes)
       })
   }
-  useEffect(() => hook, [])
+  useEffect(hook, [])
 
   console.log('render', notes.length, 'notes')
 
@@ -29,16 +46,36 @@ const App = () => {
       id: notes.length + 1,
     }
     
-    axios
-      .post('http://localhost:3001/notes', noteObject)
-      .then(response => {
-        console.log(response)
+    noteService
+      .create(noteObject)
+      .then(returnedNote  => {
+        console.log(returnedNote)
+        setNotes(notes.concat(returnedNote))
+        setNewNote('')
       })
     
     console.log('button clicked', event.target)
+  }
 
-    setNotes(notes.concat(noteObject))
-    setNewNote('')
+  const toggleImportanceOf = (id) => {
+    const note = notes.find(n => n.id === id)
+    const changedNote = { ...note, important: !note.important }
+    
+    noteService
+      .update(id, changedNote)
+      .then(returnedNote => {
+        setNotes(notes.map(n => n.id !== id ? n : returnedNote))
+      })
+      .catch(error => {
+        setErrorMessage(
+          `Note ${note.content} was already removed from server`
+        )
+        setTimeout(() => {
+          setErrorMessage(null)
+        },5000)
+        setNotes(notes.filter(n => n.id !== id))
+      })
+    console.log(`importance of ${id} needs to be toggled`)
   }
 
   const handleNoteChange = (event) => {
@@ -53,18 +90,20 @@ const App = () => {
   return (
     <div>
       <h1>Notes</h1>
+      <Notification message={errorMessage} />
       <div>
         <button onClick={() => setShowAll(!showAll)} >
         show {showAll ? "important" : 'all'}
         </button>
       </div>
       <ul>
-        {notesToShow.map(note => <Note key={note.id} note={note} />)}
+        {notesToShow.map(note => <Note key={note.id} note={note} toggleImportance={() => toggleImportanceOf(note.id)}/>)}
       </ul>
       <form onSubmit={addNote}>
         <input value={newNote} onChange={handleNoteChange} />
         <button type="submit">save</button>
       </form>
+      <Footer/>
     </div>
   )
 }
